25179553ff1f4d5c404384a156436c1b60ab5364
unknown <taylorwcolten@gmail.com>
2019-03-28 18:25:46 -0500

    fixed imports

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import {createStore} from 'redux';
 import state from "./state";
 import reducers from "./reducers";
-import { createStore } from "redux";
+
 
 var store = createStore(reducers,state);
 
